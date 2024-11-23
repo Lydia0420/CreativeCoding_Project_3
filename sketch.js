35f69f99bdab02820df9f70de3e083a146485f32
Lydia <nh2530@nyu.edu>
2024-11-23 11:00:10 -0500

    character2
    Patrick

--- sketch.js
+++ sketch.js
@@ -27,7 +27,8 @@ function draw() {
   rect(60, 120, 480, 500, 20); 
   
   //人物
-  drawMickeyColors();
+//   drawMickeyColors();
+  drawPatrickColors();
 
   // 答题
   stroke(0); 
@@ -50,24 +51,33 @@ function draw() {
   text(feedback, width / 2, 730); 
 }
 
-/1.米奇
-function drawMickeyColors() {
+// //1.米奇
+// function drawMickeyColors() {
+//   noStroke(); 
+//   fill(0); // 黑
+//   rect(200, 195, 200, 65, 10, 10, 0, 0); 
+//   fill(255, 224, 189); // 肉
+//   rect(200, 260, 200, 100); 
+//   fill(0); // 黑
+//   rect(200, 360, 200, 15); 
+//   fill(255, 0, 0); // 红
+//   rect(200, 375, 200, 90);
+//   fill(0); // 黑
+//   rect(200, 465, 200, 15); 
+//   fill(255, 215, 0); // 黄
+//   rect(200, 480, 200, 80, 0, 0, 20, 20);
+//   fill(255); // 白niukou
+//   rect(250, 397, 30, 30); // 左
+//   rect(320, 397, 30, 30); // 右
+// }
+
+//2.派大星
+function drawPatrickColors() {
   noStroke(); 
-  fill(0); // 黑
-  rect(200, 195, 200, 65, 10, 10, 0, 0); 
-  fill(255, 224, 189); // 肉
-  rect(200, 260, 200, 100); 
-  fill(0); // 黑
-  rect(200, 360, 200, 15); 
-  fill(255, 0, 0); // 红
-  rect(200, 375, 200, 90);
-  fill(0); // 黑
-  rect(200, 465, 200, 15); 
-  fill(255, 215, 0); // 黄
-  rect(200, 480, 200, 80, 0, 0, 20, 20);
-  fill(255); // 白niukou
-  rect(250, 397, 30, 30); // 左
-  rect(320, 397, 30, 30); // 右
+  fill("#FE9788");// 粉
+  rect(200, 160, 200, 420, 10); 
+  fill("#B2DB1F"); // 绿
+  rect(200, 450, 200, 85); 
 }
 
 // 检查答案是否正确
